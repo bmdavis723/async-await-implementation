a6283c682430966f07e24b8a770657f5371491c9
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-18 13:24:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+	req.end();
+})
+
+app.listen(5000, () => console.log("Listening on port 5000"));
