f951e3454ec866738aa9fcae9ad1e8f7b863d3c6
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-18 15:13:12 -0400

    async server done

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
-
+	$('#person-button').on('click', function(ev) {
+		$.ajax({
+			url: "http://localhost:5000/users",
+			data: 'json'
+		}).done(data => console.log(data));
+	})
 })
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => {
-	req.end();
-})
+app.get("/", (req, res) => {
+  res.end();
+});
 
-app.get('/users', (req, res) => {
+const url = "https://goodparts.dokku-hosted.thruhere.net/people";
 
-})
+app.get("/users", async (req, res) => {
+  ({ data } = await axios.get(url).catch(err => console.log(err)));
+  data = data.map(({ first_name, last_name, job }) => {
+    return {
+      firstName: first_name,
+      lastName: last_name,
+      job
+    };
+  });
+  res.send(data);
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
